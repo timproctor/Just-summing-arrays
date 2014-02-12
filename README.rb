69a174f06fefb73834b44127bbd8741a4cae6c87
Timothy Proctor <timproctor@users.noreply.github.com>
2014-02-12 16:31:41 -0700

    Create README.rb

---
+++ README.rb
@@ -0,0 +1,24 @@
+Just-summing-arrays
+===================
+
+Two ways to sum array
+
+# Using #inject is a short way to sum and array
+
+def total(array)
+  array.inject(:+).to_f
+end
+
+# Or.....
+
+def total(array)
+	total = 0
+	array.each do |number|
+		total += number.to_f		
+	end
+	total
+end
+
+p total([1,2,3,4,5,6,-2.3,-12]) => 6.7
+
+# Which one is more elegant Ruby?
